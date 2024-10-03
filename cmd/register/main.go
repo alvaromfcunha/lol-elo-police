84bfafc19f92197e56394362378ce6b40eea9cd6
dell <alvaro99.cunha@gmail.com>
2024-10-03 19:55:00 -0300

    fix register

--- cmd/register/main.go
+++ cmd/register/main.go
@@ -7,13 +7,14 @@ import (
 	"os/signal"
 	"syscall"
 
+	_ "github.com/mattn/go-sqlite3"
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mau.fi/whatsmeow/types/events"
 )
 
 func main() {
-	container, err := sqlstore.New("sqlite3", "file:./infrastructure/database/whatsapp.db?_foreign_keys=on", nil)
+	container, err := sqlstore.New("sqlite3", "file:infrastructure/database/whatsapp.db?_foreign_keys=on", nil)
 	if err != nil {
 		panic("cannot load whatsmeow store from sqlite file")
 	}
@@ -22,8 +23,8 @@ func main() {
 		panic("cannot retrieve registered device on whatsmeow store")
 	}
 	client := whatsmeow.NewClient(deviceStore, nil)
-	if client.Store.ID == nil {
-		panic("cannot load registered device on whatsmeow store")
+	if client.Store.ID != nil {
+		panic("device already registered")
 	}
 
 	qrChan, _ := client.GetQRChannel(context.Background())
