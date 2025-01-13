3bac8a899e1e88c2f2fe3b01fca1bfe41c1e0e2c
alvaromfcunha <alvaro99.cunha@gmail.com>
2025-01-13 15:01:24 -0300

    fix migration default values

--- infrastructure/database/migrations/20250110223210_modify_ranked_info_usage.sql
+++ infrastructure/database/migrations/20250110223210_modify_ranked_info_usage.sql
@@ -1,28 +1,28 @@
 -- +goose Up
 -- +goose StatementBegin
-ALTER TABLE matches ADD COLUMN NEW_game_creation_at DATETIME NOT NULL;
+ALTER TABLE matches ADD COLUMN NEW_game_creation_at DATETIME NOT NULL DEFAULT 0;
 UPDATE matches SET NEW_game_creation_at = game_creation_at;
 ALTER TABLE matches DROP COLUMN game_creation_at;
 ALTER TABLE matches RENAME COLUMN NEW_game_creation_at TO game_creation_at;
 
-ALTER TABLE matches ADD COLUMN NEW_game_ended_at DATETIME NOT NULL;
+ALTER TABLE matches ADD COLUMN NEW_game_ended_at DATETIME NOT NULL DEFAULT 0;
 UPDATE matches SET NEW_game_ended_at = game_ended_at;
 ALTER TABLE matches DROP COLUMN game_ended_at;
 ALTER TABLE matches RENAME COLUMN NEW_game_ended_at TO game_ended_at;
 
-ALTER TABLE ranked_info ADD COLUMN created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE ranked_info ADD COLUMN created_at DATETIME NOT NULL DEFAULT 0;
 ALTER TABLE match_participant ADD COLUMN new_ranked_info_id INTEGER REFERENCES ranked_info(id);
 ALTER TABLE match_participant ADD COLUMN prev_ranked_info_id INTEGER REFERENCES ranked_info(id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE matches ADD COLUMN NEW_game_creation_at TIMESTAMP NOT NULL;
+ALTER TABLE matches ADD COLUMN NEW_game_creation_at TIMESTAMP NOT NULL DEFAULT 0;
 UPDATE matches SET NEW_game_creation_at = game_creation_at;
 ALTER TABLE matches DROP COLUMN game_creation_at;
 ALTER TABLE matches RENAME COLUMN NEW_game_creation_at TO game_creation_at;
 
-ALTER TABLE matches ADD COLUMN NEW_game_ended_at TIMESTAMP NOT NULL;
+ALTER TABLE matches ADD COLUMN NEW_game_ended_at TIMESTAMP NOT NULL DEFAULT 0;
 UPDATE matches SET NEW_game_ended_at = game_ended_at;
 ALTER TABLE matches DROP COLUMN game_ended_at;
 ALTER TABLE matches RENAME COLUMN NEW_game_ended_at TO game_ended_at;
